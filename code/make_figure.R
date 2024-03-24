de42c283d80eb50e3f7cdd7f3a7f4081bb0ace7e
jessica-devilla <jessndev14@gmail.com>
2024-03-24 14:54:32 -0400

    explore COAD data

--- code/make_figure.R
+++ code/make_figure.R
@@ -1,12 +1,32 @@
 
-# Check if the package is already installed
+### MAKE SURE ALL REQUIREMETS ARE MET AND LOAD LIBRARIES
+
+# Check if the packages is already installed
 if (!requireNamespace("readr", quietly = TRUE)) {
   # If not installed, install it
   install.packages("readr")
 }
+# Check if the package is already installed
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+  # If not installed, install it
+  install.packages("ggplot2")
+}
 
-# Now load the library
-library(readr)
+# Check if the package is already installed
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+  # If not installed, install it
+  install.packages("dplyr")
+}
+
+# load the libraries
+suppressPackageStartupMessages({
+  library(readr)
+  library(ggplot2)
+  library(dplyr)
+})
+
+
+##### IMPORT DATA NAD FORMAT
 
 ### TEST IMPORT
 # read csv files from data folder
@@ -19,10 +39,50 @@ kraken_data <- read.csv(kraken_path)
 #import by directly calling git hub url
 kraken_data_git <- read.csv("https://media.githubusercontent.com/media/jessica-devilla/JD_20_440_pset6/main/data/Kraken-TCGA-Voom-SNM-Plate-Center-Filtering-Data.csv")
 
-# IMPORT KRAKEN DATA FROM POORE ET AL
+# import kraken data from poore et al
 kraken_url <- "https://media.githubusercontent.com/media/jessica-devilla/JD_20_440_pset6/main/data/Kraken-TCGA-Voom-SNM-Plate-Center-Filtering-Data.csv"
 kraken_data <- read_csv(url(kraken_url),col_types = cols(.default = col_character()))
+kraken_df <- as.data.frame(kraken_data, stringsAsFactors = FALSE)
 
-# IMPORT KRAKEN METADATA FROM POORE ET AL
+# import kraken metadata from poore et al
 kraken_meta_url <- "https://media.githubusercontent.com/media/jessica-devilla/JD_20_440_pset6/main/data/Metadata-TCGA-Kraken-17625-Samples.csv"
 kraken_metadata <-read_csv(url(kraken_meta_url),col_types = cols(.default = col_character()))
+kraken_metadata_df <- as.data.frame(kraken_metadata, stringsAsFactors = FALSE)
+
+# Subset the dataframe to get only values from COAD patients
+kraken_metaCOAD <- subset(kraken_metadata_df, disease_type == "Colon Adenocarcinoma")
+#dim(kraken_metaCOAD)
+#get the patient ids from COAD metadata
+ids <- kraken_metaCOAD[,1]
+kraken_COAD <- kraken_df[kraken_df[,1] %in% ids,]
+#dim(kraken_COAD) # check to see if dimensions matched
+
+#### EXPLORATORY ANALYSIS OF THE DATASET
+
+# plot stage labels
+stage_hist1 <- ggplot(kraken_metaCOAD, aes(x = pathologic_stage_label)) +
+  geom_bar() +
+  labs(x = "Pathologic Stage Label", y = "Count") +
+  ggtitle("Histogram of Pathologic Stage Labels")  +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+print(stage_hist1)
+
+# create new df which combines stage labels and plots histogram
+kraken_meta_filt <-kraken_metaCOAD %>%
+  mutate(stage_category = case_when(
+    grepl("Stage IV[A-C]*", pathologic_stage_label, ignore.case = TRUE) ~ "Stage IV",
+    grepl("Stage III[A-C]*", pathologic_stage_label, ignore.case = TRUE) ~ "Stage III",
+    grepl("Stage II[A-C]*", pathologic_stage_label, ignore.case = TRUE) ~ "Stage II",
+    grepl("Stage I[A-C]*", pathologic_stage_label, ignore.case = TRUE) ~ "Stage I",
+    TRUE ~ "Other"
+    
+  )
+      )
+
+# plot stage labels combined
+stage_hist2 <- ggplot(kraken_meta_filt, aes(x = stage_category)) +
+  geom_bar() +
+  labs(x = "Pathologic Stage Label", y = "Count") +
+  ggtitle("Histogram of Pathologic Stage Labels")  +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+print(stage_hist2)
