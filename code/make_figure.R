eb11f3f68faff52b9ed120ece6211cbc68698e03
jessica-devilla <jessndev14@gmail.com>
2024-03-23 23:24:05 -0400

    folder-structure

---
+++ code/make_figure.R
@@ -0,0 +1,8 @@
+
+# read csv files from data folder
+kraken_meta_path <- "C:\Users\jessn\Documents\MIT_G1\20_440\pset6\data\Kraken-TCGA-Voom-SNM-Plate-Center-Filtering-Data.csv"
+kraken_path <- "../data/Kraken-TCGA-Voom-SNM-Plate-Center-Filtering-Data.csv"
+
+kraken_data <- read.csv(kraken_path)
+
+
